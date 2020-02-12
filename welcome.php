93109eb4cb214e1a799664114f15fa372d19294b
Vijay19 <vijaymellempudi@gmail.com>
2020-02-11 21:57:13 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	<h5> Lastname : <?php echo $_SESSION["lastname"]; ?></h5>
 	<h5> Email : <?php echo $_SESSION["email"]; ?></h5>
 	<h5> Filename : <?php echo $_SESSION["filename"]; ?></h5>
-	<h5> Status : <?php echo $_SESSION["fileupload"]; ?></h5>
+	<h5 style="color:red;"> Status : <?php echo $_SESSION["fileupload"]; ?></h5>
         <p></p><a href='download.php?file=<?php echo $_SESSION["filename"]; ?>'> Download file </a>
 	<h5> WordCount : <?php echo $_SESSION["wordcount"]; ?></h5>
 	<form action="upload.php" method="post" enctype="multipart/form-data">
@@ -25,13 +25,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	</form>
 	<p><a href="logout.php">Logout here</a>.</p>
 </div>
-<div align="center">
-<?php
-              $fn = $_SESSION["filename"];
-              if($fn) {
-                     echo "<p><br></p><a href='download.php?file=$fn>Download file</a>";
-              }
-?>
-</div>
+
 </html>
 
