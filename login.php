d4d5f42398520ec4d17d8cc8a36189e4505fcd53
Vijay19 <vijaymellempudi@gmail.com>
2020-02-11 21:24:36 -0500

    Update login.php

--- login.php
+++ login.php
@@ -102,6 +102,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
